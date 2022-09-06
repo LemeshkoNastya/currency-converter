cda19cddc418eb46218329855762173532c50828
LemeshkoNastya <anastasia.lemeshko23@gmail.com>
2022-09-06 15:48:20 +0300

    feat: add page NotFound

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Main from '../views/Main.vue'
+import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -9,19 +10,11 @@ const routes = [{
     name: 'main',
     component: Main
   },
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  {
+    name: "notFound",
+    component: NotFound,
+    path: "*",
+  },
 ]
 
 const router = new VueRouter({
